adf2cf4b1ff02549e2a86c1e9dc5b5d5c3971feb
AnonLoverBoy17 <anonloverboy17@gmail.com>
2026-06-07 20:07:32 -0600

    MVP commit V.02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.phoenixcorp.founderfinder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
